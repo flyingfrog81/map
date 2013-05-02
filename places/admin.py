05edd80cc37198ae2e6b6555d74bd1f67b43f2e2
flyingfrog81 <marco.bartolini@gmail.com>
2013-05-02 11:02:39 +0200

    Deleted Info model and cascading changes

--- places/admin.py
+++ places/admin.py
@@ -1,16 +1,11 @@
 from django.contrib import admin
-from places.models import Place, Information
-
-class InfoInline(admin.TabularInline):
-    model = Information
-    extra = 0
+from places.models import Place
 
 class PlaceAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,               {'fields': ['name']}),
         ('Date information', {'fields': ['pub_date', 'lat', 'lon', 'description'], 'classes': ['collapse']}),    
     ]
-    inlines = [InfoInline]
     list_display = ('name', 'lat', 'lon', 'description', 'pub_date', 'was_published_recently')
     list_filter = ['pub_date']
     date_hierarchy = 'pub_date'
--- places/models.py
+++ places/models.py
@@ -1,4 +1,5 @@
 import datetime
+import settings
 from django.utils import timezone
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
@@ -7,14 +8,19 @@ class Place(models.Model):
     name = models.CharField(max_length=50)
     pub_date = models.DateTimeField('date published')
     description = models.CharField(max_length=200)
-    lat = models.FloatField(validators=[MinValueValidator(-90), MaxValueValidator(90)])
-    lon = models.FloatField(validators=[MinValueValidator(-180), MaxValueValidator(180)])
+    lat = models.FloatField(validators=[MinValueValidator(-90), 
+                                        MaxValueValidator(90)])
+    lon = models.FloatField(validators=[MinValueValidator(-180), 
+                                        MaxValueValidator(180)])
     def __unicode__(self):
         return self.name
+
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        return (self.pub_date >= 
+                timezone.now() - 
+                datetime.timedelta(days=settings.RECENT_DAYS))
+
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published recently?'
-class Information(models.Model):
-    place = models.ForeignKey(Place)
+
